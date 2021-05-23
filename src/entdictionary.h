22d60204122dea3467aee7b0457a49ff65e06194
Garux <garux@mail.ru>
2021-05-23 15:33:52 +0300

    support line comments in ent.dic, less error prone parsing

--- src/entdictionary.h
+++ src/entdictionary.h
@@ -31,26 +31,55 @@ inline KeyTypes readEntDictionary(){
         std::string string;
         std::getline( file, string, char( 0 ) );
         {
-            char *str = const_cast<char*>( string.c_str() );
-            const char *delim = " \n\r\t";
-            const char *type = "";
-            const char *token;
-            while( ( token = strtok_r( str, delim, &str ) ) ){
-                if( strcmp( token, "{" ) == 0 ){
-                    while( ( token = strtok_r( str, delim, &str ) ) ){
-                        if( strcmp( token, "}" ) == 0 ){
-                            break;
-                        }
-                        else{
-                            if( !keyTypes.emplace( token, type ).second ){
-                                fprintf(stderr, translate("Duplicate key in ent.dic: \"%s\"\n").c_str(), token );
-                            }
-                        }
+            const char *str = string.c_str();
+            const char *end = str + string.size();
+            int depth = 0;
+            NcString type;
+            NcString key;
+            const auto isSpace = [&](){ return *str == ' ' || *str == '\t'; };
+            const auto isEOL = [&](){ return *str == '\n' || *str == '\r'; };
+            const auto isDelimiter = [&](){ return isSpace() || isEOL(); };
+            const auto skipComment = [&](){
+                const bool isLineComment = strncmp( str, "//", 2 ) == 0;
+                if( isLineComment ){
+                    while( str < end && !isEOL() ){ // skip it
+                        ++str;
                     }
                 }
-                else{
-                    type = token;
+                return isLineComment;
+            };
+            const auto tryInsertKey = [&](){
+                if( depth == 1 && !key.empty() ){
+                    if( !keyTypes.emplace( key, type ).second ){
+                        fprintf(stderr, translate("Duplicate key in ent.dic: \"%s\"\n").c_str(), key.c_str() );
+                    }
+                    key.clear();
                 }
+            };
+
+            while( str < end ){
+                if( isDelimiter() ){
+                    tryInsertKey();
+                }
+                else if( skipComment() ){
+                    --str; // provide delimiter to the next iteration
+                }
+                else if( *str == '{' ){ // enter key level
+                    depth = 1;
+                    key.clear();
+                }
+                else if( *str == '}' ){ // quit to type level
+                    tryInsertKey();
+                    depth = 0;
+                    type.clear();
+                }
+                else if( depth == 0 ){ // type level
+                    type += *str;
+                }
+                else if( depth == 1 ){ // key level
+                    key += *str;
+                }
+                ++str;
             }
         }
     }
